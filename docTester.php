67f8cd777ddace4ac1dd844715c0cfdadba8af0a
SDanDyS <gameking23@hotmail.com>
2020-05-11 00:29:41 +0200

    working on image creator in JS.

---
+++ docTester.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+</head>
+<body>
+<input type="file" name="asd" id="asd"/>
+<input type="text" name="test" id="test"/>
+
+<script src="ImageCreator.js"></script>
+<script type="text/javascript">
+	new ImageCreator("asd", "test");
+</script>
+</body>
+</html>
