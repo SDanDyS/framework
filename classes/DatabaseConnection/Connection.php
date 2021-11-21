d2ea9e5aa58bc3e350b0a716efa340ed74822b08
SDanDyS <gameking23@hotmail.com>
2021-11-21 18:43:50 +0100

    add pdo extension

--- classes/DatabaseConnection/Connection.php
+++ classes/DatabaseConnection/Connection.php
@@ -1,6 +1,7 @@
 <?php
 	namespace DatabaseConnection;
 	use \mysqli;
+	use \PDO;
 	/*
 	* class Connection creates connections for the user
 	* the user can save the connections this way and easily change the localhost to the master connection
@@ -22,25 +23,42 @@
 
 		private static function getServer($key)
 		{
-			if (array_key_exists($key, self::$server)) 
+			$databaseCredentials = self::getServerCredentials($key);
+
+			//MYSQLI CONNECTION
+			$databaseConnection["mysqli"] = new mysqli(...$databaseCredentials);
+
+			if ($databaseConnection["mysqli"]->connect_error) 
+			{
+				exit("Script exit. Database error: {$databaseConnection["mysqli"]->connect_error}");
+			}
+
+			//PDO CONNECTION
+			$dsn = "mysql:host={$databaseCredentials[0]};dbname={$databaseCredentials[3]};charset=utf8mb4";
+			$options = [
+				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+
+			try {
+				$databaseConnection["pdo"] = new PDO($dsn, $databaseCredentials[1], $databaseCredentials[2], $options);
+		   } catch (\PDOException $e) {
+				throw new \PDOException($e->getMessage(), (int)$e->getCode());
+		   }
+
+		   //BOTH CONNECTIONS SUCCEEDED, SEND BACK AND LET USER DECIDE WHETHER THEY NEED MYSQLI OR PDO
+			return $databaseConnection;
+		}
+
+		private static function getServerCredentials(string $key)
+		{
+			if (array_key_exists($key, self::$server))
 			{
 				$databaseCredentials = self::$server[$key];
-
-				$databaseConnection = new mysqli(...$databaseCredentials);
-
-				if ($databaseConnection->connect_error) 
-				{
-					exit("Script exit. Database error: {$databaseConnection->connect_error}");
-				}
-				else 
-				{
-					return $databaseConnection;
-				}
+				return $databaseCredentials;
 			}
-			else 
-			{
-				exit("Script exit. Error: server {$key} could not be found and retrieved.");
-			}
+			exit("Script exit. Error: server {$key} could not be found and retrieved.");
 		}
 
 		public static function setConnection($key = NULL)
